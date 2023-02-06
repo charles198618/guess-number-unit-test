389996a45c4f0e6bab7d85165e96bb85ce8b085a
Charles Badibanga <charlesbadibanga2000@yahoo.fr>
2023-02-06 13:02:04 -0500

    Adding Scanner class and few lines of code

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.InputMismatchException;
+import java.util.Random;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hello! What is your name?");
+        String name = sc.nextLine();
+    }
+}
