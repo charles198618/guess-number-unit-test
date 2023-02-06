2b0373a3d081f3e65ed01ef232540c9e81ea4655
Charles Badibanga <charlesbadibanga2000@yahoo.fr>
2023-02-06 13:06:04 -0500

    Adding some conditional statements in the code

--- Main.java
+++ Main.java
@@ -6,5 +6,38 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         System.out.println("Hello! What is your name?");
         String name = sc.nextLine();
+        boolean playerPlaying = true;
+
+        Random rand = new Random();
+        int randomValue = 1 + rand.nextInt(20);
+        int count;
+        while (playerPlaying) {
+            count = 1;
+            System.out.println("Well, " + name + ", I am thinking of a number between 1 and 20.");
+            System.out.println("Take a guess.");
+           
+                int userInput = sc.nextInt();
+
+                while (userInput != randomValue) {
+                    count++;
+                    if (userInput > randomValue) {
+                        System.out.println("Your guess is too high.");
+                    } else {
+                        System.out.println("Your guess is too low.");
+                    }
+                    System.out.println("Take a guess.");
+                    userInput = sc.nextInt();
+                }
+                System.out.println("Good job, " + name + "! You guessed my number in " + count + " guesses!");
+
+                System.out.println("Would you like to play again? (y or n)");
+                String playResponse = sc.next();
+                playerPlaying = !(playResponse.startsWith("N") || playResponse.startsWith("n"));
+
+                System.out.println("You entered an Invalid input, please start over!");
+                break;
+
+        }
     }
 }
+
